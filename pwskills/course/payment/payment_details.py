4284d5ea34d00753042d263aa1ae15d7704dbaea
tejas05in <103567190+tejas05in@users.noreply.github.com>
2023-02-12 00:06:48 +0530

    final

---
+++ pwskills/course/payment/payment_details.py
@@ -0,0 +1,8 @@
+#import os
+#import sys
+#from os.path import dirname,join,abspath
+#sys.path.insert(0,abspath(join(dirname(__file__),"..")))
+#from course import course_details
+def payment():
+    print("this is my payment file")
+#course_details.course()
